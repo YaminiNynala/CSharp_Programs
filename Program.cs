8172eb809d167810837fe7e22966424404ee1810
YaminiNynala <114306752+YaminiNynala@users.noreply.github.com>
2023-04-27 18:35:28 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,16 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace printMessage
+namespace ReadTextFile
 {
     class Program
     {
         static void Main(string[] args)
         {
-            PlainText.printMsgViaText();
-            UserDetails.displayUserDetails();
-            DynamicData.userInput();
-        }
+            //ReadFile r1 = new ReadFile("E:/C#_Programs/printMessage/TxtDoc.txt");
+            //ReadFile.readFileInConsole();
 
+            //ArrayAndListPractice.arrayData();
+            //Console.WriteLine("*****************************");
+            //ArrayAndListPractice.listData();
+
+            string filePath = "E:/C#_Programs/ReadTextFile/data.txt";
+            Array a1 = new Array(filePath);
+            Array.readArray();
+
+            //List1 l1 = new List1(filePath);
+            //List1.readList();
+
+            //DataInTable d1 = new DataInTable(filePath);
+            //DataInTable.readListInTable();
+
+            Console.ReadKey();
+
+        }
     }
 }
